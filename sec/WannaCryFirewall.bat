0816fea2bc65dae3f3115dc525bc84bc0967ca15
tiankonguse <i@tiankonguse.com>
2017-05-16 01:00:27 +0800

    update

--- sec/WannaCryFirewall.bat
+++ sec/WannaCryFirewall.bat
@@ -5,23 +5,23 @@ rem http://www.iuqerfsodp9ifjaposdfjhgosurijfaewrwergwea.com
 
 
 ECHO open firewall
-rem netsh firewall set opmode enable
-rem netsh advfirewall set allprofiles state on
+netsh firewall set opmode enable
+netsh advfirewall set allprofiles state on
 
-rem netsh advfirewall firewall add rule name="Deny Port 445" dir=in action=block protocol=TCP localport=445
-rem netsh firewall set portopening  protocol=TCP port=445 mode=disable name=deny445
+netsh advfirewall firewall add rule name="Deny Port 445" dir=in action=block protocol=TCP localport=445
+netsh firewall set portopening  protocol=TCP port=445 mode=disable name=deny445
 ECHO close 445 port
 
-rem netsh advfirewall firewall add rule name="Deny Port 137" dir=in action=block protocol=TCP localport=137
-rem netsh firewall set portopening  protocol=TCP port=137 mode=disable name=deny137
+netsh advfirewall firewall add rule name="Deny Port 137" dir=in action=block protocol=TCP localport=137
+netsh firewall set portopening  protocol=TCP port=137 mode=disable name=deny137
 ECHO close 137 port
 
-rem netsh advfirewall firewall add rule name="Deny Port 138" dir=in action=block protocol=TCP localport=138
-rem netsh firewall set portopening  protocol=TCP port=138 mode=disable name=deny138
+netsh advfirewall firewall add rule name="Deny Port 138" dir=in action=block protocol=TCP localport=138
+netsh firewall set portopening  protocol=TCP port=138 mode=disable name=deny138
 ECHO close 138 port
 
-rem netsh advfirewall firewall add rule name="Deny Port 139" dir=in action=block protocol=TCP localport=139
-rem netsh firewall set portopening  protocol=TCP port=139 mode=disable name=deny139
+netsh advfirewall firewall add rule name="Deny Port 139" dir=in action=block protocol=TCP localport=139
+netsh firewall set portopening  protocol=TCP port=139 mode=disable name=deny139
 ECHO close 139 port
 
 ECHO port all close

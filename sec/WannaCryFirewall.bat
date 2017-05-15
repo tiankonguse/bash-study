42581a05fc0f0a3fc9ed2353ac0ca851fa3a4b05
tiankonguse <i@tiankonguse.com>
2017-05-16 01:43:37 +0800

    update

--- sec/WannaCryFirewall.bat
+++ sec/WannaCryFirewall.bat
@@ -5,26 +5,17 @@ rem http://www.iuqerfsodp9ifjaposdfjhgosurijfaewrwergwea.com
 
 
 ECHO open firewall
-netsh firewall set opmode enable
+
+rem old cmd open firewall
+rem netsh firewall set opmode enable
+
+rem new cmd open firewall
+netsh advfirewall set currentprofile state on
 netsh advfirewall set allprofiles state on
 
-netsh advfirewall firewall add rule name="Deny Port 445" dir=in action=block protocol=TCP localport=445
-netsh firewall set portopening  protocol=TCP port=445 mode=disable name=deny445
-ECHO close 445 port
+netsh advfirewall firewall add rule name="Deny WannaCry port" dir=in action=block protocol=TCP localport=445,137,138,139
+ECHO close 445,137,138,139 port
 
-netsh advfirewall firewall add rule name="Deny Port 137" dir=in action=block protocol=TCP localport=137
-netsh firewall set portopening  protocol=TCP port=137 mode=disable name=deny137
-ECHO close 137 port
-
-netsh advfirewall firewall add rule name="Deny Port 138" dir=in action=block protocol=TCP localport=138
-netsh firewall set portopening  protocol=TCP port=138 mode=disable name=deny138
-ECHO close 138 port
-
-netsh advfirewall firewall add rule name="Deny Port 139" dir=in action=block protocol=TCP localport=139
-netsh firewall set portopening  protocol=TCP port=139 mode=disable name=deny139
-ECHO close 139 port
-
-ECHO port all close
 
 PAUSE
 
